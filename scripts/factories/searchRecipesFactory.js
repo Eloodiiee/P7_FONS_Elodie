e4790d47ef98ac4800fd9244a990e02814ee6677
Fons Elodie <elodie.fons66@gmail.com>
2023-11-28 16:31:00 +0100

    Filtres des ustensiles/ingrédients

--- scripts/factories/searchRecipesFactory.js
+++ scripts/factories/searchRecipesFactory.js
@@ -5,14 +5,25 @@ export function searchRecipes(recipes) {
     let recipesFiltered = []
     for (let i = 0; i < recipes.length; i++) {
         const regexQuery = new RegExp(`${recipeRequest}`)
-        if (recipeRequest == "") {
+        let ustensils = recipes[i].ustensils
+        let ingredients = recipes[i].ingredients
+        if (recipeRequest == "" || regexQuery.test(recipes[i].name.toUpperCase()) || regexQuery.test(recipes[i].appliance.toUpperCase())) {
             recipesFiltered.push(recipes[i])
         }
-        if (regexQuery.test(recipes[i].name.toUpperCase())) {
-            recipesFiltered.push(recipes[i])
+        for (let i = 0; i < ingredients.length; i++) {
+            const ingredient = ingredients[i].ingredient.toUpperCase()
+
+            if (regexQuery.test(ingredient)) {
+                recipesFiltered.push(recipes[i])
+            }
         }
-        if (regexQuery.test(recipes[i].appliance.toUpperCase())) {
-            recipesFiltered.push(recipes[i])
+        for (let i = 0; i < ustensils.length; i++) {
+            const ustensil = ustensils[i].toUpperCase()
+            console.log(ustensil)
+
+            if (regexQuery.test(ustensil)) {
+                recipesFiltered.push(recipes[i])
+            }
         }
     }
     recipesFiltered = recipesFiltered.filter((item, index) => recipesFiltered.indexOf(item) === index)
