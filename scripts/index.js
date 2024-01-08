3ad8b9e06b24c0dd72ba91d89adf8fb4dbc0969b
Fons Elodie <elodie.fons66@gmail.com>
2024-01-08 16:01:11 +0100

    réécriture du code de index.js, modification style.css et index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,15 @@
 import { recipes } from "../data/recipes.js"
 import { displayCards } from "./factories/cardFactory.js"
-import { searchByIngredient } from "./utils/searchByFilter.js"
-import { searchByAppliance } from "./utils/searchByFilter.js"
-import { searchByUstensil } from "./utils/searchByFilter.js"
-import { searchRecipes } from "./utils/searchRecipes.js"
-import { createTag } from "./utils/createTag.js"
 
-let card = []
-let allCards = []
-let recipesJSON = []
-let recipesFiltered = []
-let recipeRequest = ""
-let nbOfRecipe = 0
-const inputSearchBar = document.querySelector("#inputSearchBar")
-const recipesContainer = document.querySelector(".recipesContainer")
-const numberOfRecipes = document.querySelector(".number_of_recipes")
-const tagsContainer = document.querySelector(".tags")
+let card = [] // Initialisation de la variable qui stocke les donnés de chaque carte recette une par une.
+let allCards = [] // Initialisation de la variable qui récupère toutes les cartes de recettes.
+let nbOfRecipe = 0 // Variable qui permet de stocker le nombre de recettes pour pouvoir l'afficher
 
-recipesJSON = JSON.stringify(recipes)
-recipesJSON = JSON.parse(recipesJSON)
+const recipesContainer = document.querySelector(".recipesContainer") // Je sélectionne le container des recettes
+const numberOfRecipes = document.querySelector(".number_of_recipes") // Je sélectionne le span du nombre de recettes
+
+/** Function fillContainer qui permet d'afficher toutes les cartes de recette 
+    au chargement de la page et en fonction du résultat de la recherche. **/
 
 function fillContainer(recipesToBeDisplayed) {
     recipesContainer.innerHTML = ""
@@ -27,87 +18,21 @@ function fillContainer(recipesToBeDisplayed) {
         recipesContainer.appendChild(card)
         allCards.splice(0, 0, card)
     })
-    allCards.reverse()
-    console.log(allCards)
     nbOfRecipe = allCards.length
     displayNumberOfRecipe(nbOfRecipe)
 }
-fillContainer(recipesJSON)
 
-inputSearchBar.addEventListener("input", (e) => {
-    recipeRequest = e.target.value.toLowerCase()
-    if (inputSearchBar.value.length > 2) {
-        searchBy(recipeRequest, searchRecipes)
-    }
-    if (inputSearchBar.value.length == 0) {
-        searchBy(recipeRequest, searchRecipes)
-    }
-})
-function searchBy(recipeRequest, functionSearch) {
-    recipesFiltered = functionSearch(recipeRequest, recipesJSON)
-    allCards = []
-    fillContainer(recipesFiltered)
-}
+/** Exécution de la funtion fillContainer au chargement de la page **/
+fillContainer(recipes)
+
+/** Function displayNumberOfRecipe qui permet d'afficher le nombre de recettes en temps réel
+    Cette function se base sur le nombre de recettes de recipes.js **/
 
 function displayNumberOfRecipe(nbOfRecipe) {
-    if (nbOfRecipe == 50) {
-        numberOfRecipes.textContent = `${nbOfRecipe + 1450} recettes`
-    } else if (nbOfRecipe == 1) {
-        numberOfRecipes.textContent = `${nbOfRecipe} recette`
+    numberOfRecipes.textContent = `${nbOfRecipe} recettes`
+    if (nbOfRecipe == 1) {
+        numberOfRecipes.textContent = "1 recette"
     } else if (nbOfRecipe == 0) {
         numberOfRecipes.textContent = "Aucune recette"
-    } else {
-        numberOfRecipes.textContent = `${nbOfRecipe} recettes`
     }
 }
-
-function closeTag() {
-    if (tagsContainer.childNodes.length > 0) {
-        tagsContainer.childNodes.forEach((tag) => {
-            const tagCloseBtn = tag.lastChild
-            tagCloseBtn.addEventListener("click", () => {
-                searchBy("", searchRecipes)
-                tag.remove()
-            })
-        })
-    }
-}
-
-const advancedFilters = document.querySelectorAll(".advancedFilter")
-
-advancedFilters.forEach((advancedFilter) => {
-    const chevron = advancedFilter.querySelector(".fa-chevron-down")
-    const filterList = advancedFilter.querySelector(".filterSearchList")
-    const inputTags = advancedFilter.querySelectorAll(".input-tag")
-    const liLists = advancedFilter.querySelectorAll(".li-list")
-    advancedFilter.children[0].addEventListener("click", () => {
-        chevron.classList.toggle("rotate")
-        filterList.classList.toggle("open")
-        inputTags.forEach((inputTag) => {
-            let filterList = []
-            if (inputTag.id == "ingredients") {
-                recipesJSON.filter((recipe) => recipe.ingredients.forEach((ing) => filterList.push(ing.ingredient)))
-            }
-            if (inputTag.id == "appliances") {
-                recipesJSON.filter((recipe) => filterList.push(recipe.appliance))
-            }
-            if (inputTag.id == "ustensils") {
-                recipesJSON.filter((recipe) => recipe.ustensils.forEach((ust) => filterList.push(ust)))
-            }
-            filterList = [...new Set(filterList)]
-            liLists.forEach((liList) => {
-                filterList.forEach((filter) => {
-                    const li = document.createElement("li")
-                    li.textContent = filter
-                    liList.appendChild(li)
-                    li.addEventListener("click", (e) => {
-                        recipeRequest = e.target.innerText.toLowerCase()
-                        createTag(recipeRequest)
-                        searchBy(recipeRequest, searchRecipes)
-                        closeTag()
-                    })
-                })
-            })
-        })
-    })
-})
