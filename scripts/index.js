602814ebf6c675b528ec27338b50fcd075f2c1e9
Fons Elodie <elodie.fons66@gmail.com>
2024-01-09 16:15:42 +0100

    barre de recherche principale, function de recherche et commentaires créés

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
 import { recipes } from "../data/recipes.js"
 import { displayCards } from "./factories/cardFactory.js"
+import { searchRecipes } from "./utils/searchRecipes.js"
 
 let card = [] // Initialisation de la variable qui stocke les donnés de chaque carte recette une par une.
 let allCards = [] // Initialisation de la variable qui récupère toutes les cartes de recettes.
-let nbOfRecipe = 0 // Variable qui permet de stocker le nombre de recettes pour pouvoir l'afficher
+let nbOfRecipe = 0 // Variable qui permet de stocker le nombre de recettes pour pouvoir l'afficher.
+let recipesFiltered = [] // Permet de stocker les recettes filtrés.
+let recipeRequest = "" // Requête de recherche des recettes.
 
-const recipesContainer = document.querySelector(".recipesContainer") // Je sélectionne le container des recettes
-const numberOfRecipes = document.querySelector(".number_of_recipes") // Je sélectionne le span du nombre de recettes
+const recipesContainer = document.querySelector(".recipesContainer") // Je sélectionne le container des recettes.
+const numberOfRecipes = document.querySelector(".number_of_recipes") // Je sélectionne le span du nombre de recettes.
+const inputSearchBar = document.querySelector("#inputSearchBar") // Je sélectionne la barre de recherche principale.
 
-/** Function fillContainer qui permet d'afficher toutes les cartes de recette 
-    au chargement de la page et en fonction du résultat de la recherche. **/
-
+/** Function fillContainer qui permet d'afficher toutes les cartes de recette, **/
+/** au chargement de la page et en fonction du résultat de la recherche. **/
 function fillContainer(recipesToBeDisplayed) {
     recipesContainer.innerHTML = ""
     recipesToBeDisplayed.forEach((recipe) => {
@@ -25,9 +28,8 @@ function fillContainer(recipesToBeDisplayed) {
 /** Exécution de la funtion fillContainer au chargement de la page **/
 fillContainer(recipes)
 
-/** Function displayNumberOfRecipe qui permet d'afficher le nombre de recettes en temps réel
-    Cette function se base sur le nombre de recettes de recipes.js **/
-
+/** Function displayNumberOfRecipe qui permet d'afficher le nombre de recettes en temps réel **/
+/** Cette function se base sur le nombre de recettes de recipes.js **/
 function displayNumberOfRecipe(nbOfRecipe) {
     numberOfRecipes.textContent = `${nbOfRecipe} recettes`
     if (nbOfRecipe == 1) {
@@ -36,3 +38,21 @@ function displayNumberOfRecipe(nbOfRecipe) {
         numberOfRecipes.textContent = "Aucune recette"
     }
 }
+/** La function searchBy prend deux paramètres : recipeRequest qui une chaîne de caractères représentant la requête de recherche, **/
+/** et la functionSearch qui est une function variable qui s'adapte en fonction de celle donnée dans les paramètres. **/
+/** Appel de la functionSearch en lui passant recipeRequest et les recettes. **/
+/** Le résultat de cette recherche est stocké dans recipesFiltered. **/
+/** Réinitialise allCards pour nettoyer la liste des cartes. **/
+/** Appelle la fonction fillContainer en lui passant les recettes filtrées. **/
+function searchBy(recipeRequest, functionSearch) {
+    recipesFiltered = functionSearch(recipeRequest, recipes)
+    allCards = []
+    fillContainer(recipesFiltered)
+}
+/** Effectue la recherche si la longueur de la chaîne est supérieure à 2 ou si l'utilisateur appuie sur Backspace. **/
+inputSearchBar.addEventListener("input", (e) => {
+    recipeRequest = e.target.value.toLowerCase()
+    if (inputSearchBar.value.length > 2 || e.inputType === "deleteContentBackward") {
+        searchBy(recipeRequest, searchRecipes)
+    }
+})
